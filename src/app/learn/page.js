ed951c14e7ccb979d2bc6ef1e4f487c735b07d4d
marangaa <rchdmaranga@gmail.com>
2024-08-18 03:41:40 +0300

    feat: Add @radix-ui/react-accordion dependency and update EducationPage component
    This commit adds the "@radix-ui/react-accordion" dependency to the package.json file, with version "^1.2.0". It also updates the EducationPage component in the src/app/learn/page.js file to import and use the new Education component from "@/components/component/education-page".

--- src/app/learn/page.js
+++ src/app/learn/page.js
@@ -1,27 +1,5 @@
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import {ModuleOverview} from "@/components/component/module-overview"
-import {VideoFeed} from "@/components/component/video-feed"
+import Education from '@/components/component/education-page';
 
-export default function CivicEducationPage() {
-    return (
-        <div>
-            <h1>Civic Education</h1>
-            <Tabs defaultValue="overview">
-                <TabsList>
-                    <TabsTrigger value="overview">Overview</TabsTrigger>
-                    <TabsTrigger value="modules">Modules</TabsTrigger>
-                    <TabsTrigger value="videos">Short Videos</TabsTrigger>
-                </TabsList>
-                <TabsContent value="overview">
-                    <p>Welcome to Civic Education. Here you can learn about...</p>
-                </TabsContent>
-                <TabsContent value="modules">
-                    <ModuleOverview />
-                </TabsContent>
-                <TabsContent value="videos">
-                    <VideoFeed />
-                </TabsContent>
-            </Tabs>
-        </div>
-    )
+export default function EducationPage() {
+    return <Education />;
 }
