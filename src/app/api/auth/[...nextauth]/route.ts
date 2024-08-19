c16650fe94ab446af0c1d04496da95a404878a17
marangaa <rchdmaranga@gmail.com>
2024-08-19 22:29:53 +0300

    refactor: Remove unused import in route.ts file

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,6 @@ import NextAuth from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
 import prisma from '@/lib/prisma'
 import { compare } from 'bcryptjs'
-import { User } from 'next-auth'
 
 const handler = NextAuth({
     providers: [
